390a92d029b31318576725e78f8b294849ec6871
termi-hub-app <143942670+liveweeeb13@users.noreply.github.com>
2025-04-08 19:49:46 +0200

    s

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,7 +27,7 @@ function setCookie(name, value, days) {
   const d = new Date();
   d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
   let expires = "expires=" + d.toUTCString();
-  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+  document.cookie = name + "=" + encodeURIComponent(value) + ";" + expires + ";path=/";
 }
 
 // Get cookies
@@ -37,7 +37,7 @@ function getCookie(name) {
   for (let i = 0; i < ca.length; i++) {
     let c = ca[i];
     while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    if (c.indexOf(nameEQ) == 0) return decodeURIComponent(c.substring(nameEQ.length, c.length));
   }
   return null;
 }
